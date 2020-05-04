17e1e49e0f3f196d4d369e8b7a03a4029abb6677
michellesandhika <58082740+michellesandhika@users.noreply.github.com>
2020-05-04 21:33:24 +0700

    commit on master

--- src/edd.C
+++ src/edd.C
@@ -1,157 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "utility.c"
 
 int main(){
 
+    int read_from_file(order ordersread[]);
+
     int no_plants = 3; //plants are also named into plant 0, plant 1, plant 2 etc
     int plants_orders[3] = {300,400,500}; //how many orders they can do in a day
-    int total_order; //total orders
+    int total_order=0; //total orders
     char buf_in[100];
     char buf_out[100];
-    
-    struct order{
-        int arrival_date;
-        char order_number[10];
-        int due_date;
-        int quantity;
-        char product_name;
-    }
 
-    //read from file to get the total_order
-    total_order=;
-
-    struct order ordersread[total_order];
+    //need to think of how to make this 
+    order ordersread[200];
 
     //read orders from file
-    read_from_file(ordersread);
+    total_order = read_from_file(ordersread);
 
-    //for the pipes
-    int ptoc[10][2];
-    int ctop[10][2];
+    for (int i=0; i< total_order; i++){
+        printf("%d",ordersread[i].arrival_date);
+        printf("%s",ordersread[i].order_number);
+        printf("%s",ordersread[i].due_date);
+        printf("%d",ordersread[i].quantity);
+        printf("%s",ordersread[i].product_name);
 
-    //creating pipes
-    for (int i=0; i<no_plants; i++){
-        pipe(ptoc[i]);
-        pipe(ctop[i]);
     }
 
-    for(int a=0;a<no_plants;a++) 
-    {
-        
-        if(fork() == 0) 
-        { 
-            int total_days=0; //how many days have passed
-            char *plant_order[endtime_of_the_plants][total_order];
 
-            close(ptoc[a][1]); /* close child out */
-            close(ctop[a][0]);
+    // int total_days=0; //how many days have passed
+    // char *plant_order[endtime_of_the_plants][total_order];
 
+    // //write all the orders the parent 
+    // for (int i=0; i< total_days; i++){
+    //     write(ctop[a][1],buf_in, 10); 
+    // }
 
-            while(true){
-                read(ptoc[a][0],buf_in, 10); //read from parent for what order they have to process
+    // int day_now=0; //start the first day with 0
+    // struct order order_now; 
+    // char processed_order[no_plants][total_order];
+    // int plant_now;
+    // int count_assigned=0;
+    // int orders_unfinished;
+    // struct order queue[total_order];
+    // char order_name[10];
 
-                if (strcmp(buf_in, "done")==0){
-                    break;
-                }
+    // for (int i=0; i<total_order; i++){
 
-                //places it in the order
-                plant_order[total_days]=buf_in;
+    //     //if there is an order that just arrived
+    //     if (new_arrival){
+    //        edd_queue(struct order queue);
+    //     }
 
-            }
+    //     order_now= queue[0];
+    //     order_name = order_now.order_number;
+    //     order_amount= order_now.quantity;
+    //     orders_unfinished=order_amount;
+    //     remove_from_queue(queue[0]);
 
-            //write all the orders the parent 
-            for (int i=0; i< total_days; i++){
-                write(ctop[a][1],buf_in, 10); 
-            }
+    //     //while not all the plants are assigned yet
+    //     while (count_assigned <3){ 
 
-        }
-
-        close(ptoc[a][0]); /* close child out */
-        close(ctop[a][1]);
-        exit(0);
-    }
-
-    int day_now=0; //start the first day with 0
-    struct order order_now; 
-    char processed_order[no_plants][total_order];
-    int plant_now;
-    int count_assigned=0;
-    int orders_unfinished;
-    struct order queue[total_order];
-    char order_name[10];
-
-    for (int i=0; i<total_order; i++){
-
-        //if there is an order that just arrived
-        if (new_arrival){
-           edd_queue(struct order queue);
-        }
-
-        order_now= queue[0];
-        order_name = order_now.order_number;
-        order_amount= order_now.quantity;
-        orders_unfinished=order_amount;
-        remove_from_queue(queue[0]);
-
-        //while not all the plants are assigned yet
-        while (count_assigned <3){ 
-
-            if (orders_unfinished==0){
-                order_name = queue[0].order_number;
-                order_amount= queue[0].quantity;
-                orders_unfinished=order_amount;
-                remove_from_queue(queue[0]);
-            }
+    //         if (orders_unfinished==0){
+    //             order_name = queue[0].order_number;
+    //             order_amount= queue[0].quantity;
+    //             orders_unfinished=order_amount;
+    //             remove_from_queue(queue[0]);
+    //         }
             
-            //assigning the order to the different plants
+    //         //assigning the order to the different plants
             
-            /* havent figure out the code yet hahahha 
-                should be related to best fit algorithm though
-            */
-        }
+    //         /* havent figure out the code yet hahahha 
+    //             should be related to best fit algorithm though
+    //         */
+    //     }
 
-        //if the orders is still unfinished 
-        if (orders_unfinished>0){
-            order_now.quantity=orders_unfinished;
-            add_to_beginning(order_now);
-        }
-        count_assigned=0;
+    //     //if the orders is still unfinished 
+    //     if (orders_unfinished>0){
+    //         order_now.quantity=orders_unfinished;
+    //         add_to_beginning(order_now);
+    //     }
+    //     count_assigned=0;
 
-    }
+    // }
         
 }
 
 /* read from the file*/
-void read_from_file(struct order ordersread[]){
+int read_from_file(order ordersread[]){
+
+    int total_order=0;
+    char temp[40];
+    char *quant_temp;
+
+    FILE *in_file = fopen("orders.txt", "r");   // open read file
     
+    if (in_file == NULL) {
+        printf("Error in opening files\n");
+        exit(1);
+    }
+    
+    while (fgets(temp, CMD_LENGTH, in_file)) {  // while not EOF
+        temp[strlen(temp) - 1] = 0;     // remove new line from temp
+        //split the file
+
+        ordersread[total_order].arrival_date= temp[0]-48;
+        substring(temp, ordersread[total_order].order_number,2,5);
+        substring(temp, quant_temp, 7,15);
+        printf("%s", quant_temp);
+        
+    
+        substring(temp, quant_temp,19,24);
+        ordersread[total_order].quantity=atoi(quant_temp);
+        substring(temp, ordersread[total_order].product_name,25,35);
+
+        total_order++;
+        
+    }
+    fclose(in_file);
+    return total_order;
 
 }
 
-/* what is the number of products in this order*/
-void amount_of_order(char order_now){
+// /* what is the number of products in this order*/
+// void amount_of_order(char order_now){
 
-}
+// }
 
-/*look if there is an arrival at this day*/
-void new_arrival(int day_now){
+// /*look if there is an arrival at this day*/
+// void new_arrival(int day_now){
 
-}
+// }
 
-/*creates the queue*/
-void edd_queue(struct order order_now){
-}
+// /*creates the queue*/
+// void edd_queue(struct order order_now){
+// }
 
 
-int free_index(){
-    //finding out when is the next free space
-}
+// int free_index(){
+//     //finding out when is the next free space
+// }
 
-void add_to_beginning(){
-    //add to beginning of the queue
-}
+// void add_to_beginning(){
+//     //add to beginning of the queue
+// }
 
-void remove_from_queue(){
-    //remove from the beginning of the queue
+// void remove_from_queue(){
+//     //remove from the beginning of the queue
 
-}
+// }
