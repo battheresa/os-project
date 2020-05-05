90c1d6a484e5007bb73bb5e61d8c112488ab3f32
battheresa <chawisa.batt@gmail.com>
2020-05-05 23:53:57 +0700

    add reference format for the report

--- src/outputModule.c
+++ src/outputModule.c
@@ -28,9 +28,70 @@ void printREPORT(char filename[]) {
         exit(1);
     }
     
+    // read from raw data file
     // write to file
     //fprintf(report_file, "%d %s\n", time, data)
     
     fclose(report_file);
 }
 
+/*
+========================================================================================== (90)
+
+Plant X (300 per day)
+[period range]
+
+Date            Product Name        Order Number        Quantity        Due Date
+YYYY-MM-DD      Product_A           P_XXX               XXXX            YYYY-MM-DD
+
+------------------------------------------------------------------------------------------ (90)
+
+Plant Y (400 per day)
+[period range]
+ 
+...
+ 
+========================================================================================== (90)
+  
+Analysis Report
+       
+------------------------------------------------------------------------------------------ (90)
+  
+Algorithm: EDD (Earliest Due Date)
+ 
+Order Accepted (total of xxx orders):
+
+Product Name        Quantity        Start           Finish          Duration        Plant
+P_XXX               XXXX            YYYY-MM-DD      YYYY-MM-DD      XXXX            XXXX
+
+Order Rejected (total of xxx orders):
+  
+Product Name        Order Number    Quantity        Due Date
+P_XXX               P_XXX           XXXX            YYYY-MM-DD
+  
+------------------------------------------------------------------------------------------ (90)
+  
+Algorithm: SJF (Shortest Job First)
+
+...
+
+========================================================================================== (90)
+
+Performance Report
+ 
+Algorithm: EDD (Earliest Due Date)
+
+Plant_X:    Number of days in use               XXX days
+            Number of products produced         XXX (in total)
+            Utilization of the plant            XX.XX %
+
+Overal utilization                              XX.XX %
+ 
+------------------------------------------------------------------------------------------ (90)
+
+Algorithm: SJF (Shortest Job First)
+ 
+...
+ 
+========================================================================================== (90)
+*/
